9feae52497f69f682c759f6fef56cc2ed7e50f41
Sky_Time87 <blackshadow1609@yandex.ru>
2026-01-27 18:36:31 +0300

    switch-background DONE

--- JavaScript/02-events/events.js
+++ JavaScript/02-events/events.js
@@ -1,24 +1,32 @@
-// JavaScript source code
+п»ї// JavaScript source code
 function setImage() {
     let filename = document.getElementById("image-file");
+
+    if (filename.files.length === 0) {
+        return; // РќРёС‡РµРіРѕ РЅРµ РІС‹Р±СЂР°РЅРѕ
+    }
+
     let reader = new FileReader();
     reader.onload = function (e) {
         document.getElementById("photo").src = e.target.result;
     }
     reader.readAsDataURL(filename.files[0]);
 }
+
 function setBackground() {
     let color_tool = document.getElementById('choose-color');
     let color = color_tool.value;
-    //document.body.style.backgroundImage = null;
-    //document.body.style.backgroundColor = color;
     document.getElementById('color-sample').style.backgroundColor = color;
     document.getElementById('color-sample').style.width = "200px";
     document.getElementById('color-sample').style.height = "200px";
 }
+
 function switchBackground() {
+    let delay = document.getElementById("delay").value;
+
+    document.body.style.transition = `color ${delay}s, background ${delay}s`;
+
     const imgElement = document.getElementById("switch-background");
-    const currentFile = imgElement.src.split('/').pop();
 
     if (document.body.classList.contains('white')) {
         document.body.classList.remove('white');
@@ -30,18 +38,3 @@ function switchBackground() {
         imgElement.src = 'image/moon.png';
     }
 }
-function setImage() {
-    let filename = document.getElementById("image-file");
-
-    if (filename.files.length === 0) {
-        return; // Ничего не выбрано
-    }
-
-    let reader = new FileReader();
-    reader.onload = function (e) {
-        document.getElementById("photo").src = e.target.result;
-    }
-    reader.readAsDataURL(filename.files[0]);
-}
-
-
